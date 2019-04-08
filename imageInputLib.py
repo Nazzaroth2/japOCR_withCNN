e68792455fb8ef7cc589520f1276860c0d9fd974
Nazzaroth2 <cernyviktor@web.de>
2019-04-08 10:17:50 +0200

    inital Files for testing of commiting

---
+++ imageInputLib.py
@@ -0,0 +1,6 @@
+"""
+author: Viktor Cerny
+created: 08-04-19
+last edit: 08-04-19
+desc: library for functions that deal with image loading and pixel data extraction etc.
+"""
---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+author: Viktor Cerny
+created: 08-04-19
+last edit: 08-04-19
+desc: main file for the project. starts up the program
+"""

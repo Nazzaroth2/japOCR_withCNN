1c6b552dc450d83de10d5233ce1ef8c92f2ba8eb
Nazzaroth2 <cernyviktor@web.de>
2019-04-20 11:46:24 +0200

    Milestone 1 in v1.0 reached!
    i now have the whole desktop pixel data interface implemented.
    only one function call to get from window raw pixel data to a pytorch tensor.

--- imageInputLib.py
+++ imageInputLib.py
@@ -6,7 +6,13 @@ desc: library for functions that deal with image loading and pixel data extracti
 """
 
 import win32gui as gui
+import win32ui
+import win32con
 from time import sleep
+import numpy as np
+from PIL import Image
+from torchvision.transforms import ToTensor
+
 
 #desc: returns the handle of the active window
 def getWindowHandle():
@@ -27,4 +33,55 @@ def captureTargetWindow():
             return actHandle
             break
 
-        sleep(1)
+        sleep(0.5)
+
+#desc: calculates the position of targeted window given a handle
+def getTargetWindowPosition(winHandle):
+    targetPosition = gui.GetWindowRect(winHandle)
+
+    return targetPosition
+
+#desc: wrapper to the get targetWindowPosition Call
+def returnTargetWindowPosition():
+    return getTargetWindowPosition(captureTargetWindow())
+
+#desc: returns a tuple of 4channel pixel values of target window space
+#order is BGR-Alpha
+def returnRawTarget():
+    #get window space
+    taskWindowValues = returnTargetWindowPosition()
+
+    taskX = taskWindowValues[0]
+    taskY = taskWindowValues[1]
+    taskW = taskWindowValues[2] - taskX
+    taskH = taskWindowValues[3] - taskY
+
+    # get the desktop
+    hwin = gui.GetDesktopWindow()
+    hwindc = gui.GetWindowDC(hwin)
+
+    # do other necessery stuff (should figure out eventually what all this stuff does)
+    srcdc = win32ui.CreateDCFromHandle(hwindc)
+    memdc = srcdc.CreateCompatibleDC()
+    bmp = win32ui.CreateBitmap()
+    bmp.CreateCompatibleBitmap(srcdc, taskW, taskH)
+    memdc.SelectObject(bmp)
+
+    # copy desktop region into bmp object
+    memdc.BitBlt((0, 0), (taskW, taskH), srcdc, (taskX, taskY), win32con.SRCCOPY)
+
+    return bmp.GetInfo(), bmp.GetBitmapBits()
+
+#desc: give the image out in a torch Tensor format to be fed right into the net
+#conversion-line: raw -> np.array -> pil.Image -> torch.Tensor
+def convertRawToTensor():
+    rawInfo, rawPixels = returnRawTarget()
+
+    array = np.asarray(rawPixels, dtype=np.uint8)
+
+    pil_im = Image.frombuffer('RGB', (rawInfo['bmWidth'], rawInfo['bmHeight']),array, 'raw', 'BGRX', 0, 1)
+
+
+    tensor = ToTensor().__call__(pic=pil_im)
+
+    return tensor
--- main.py
+++ main.py
@@ -5,14 +5,9 @@ last edit: 08-04-19
 desc: main file for the project. starts up the program
 """
 
-import win32gui as gui
-
-import time
-
 import imageInputLib as imgLib
 
-actHandle = imgLib.captureTargetWindow()
+tensor = imgLib.convertRawToTensor()
 
-print(actHandle)
-
+print(tensor.size())
 
